56458c5c6f29f27a03473770b4899394aa4dcab4
RJRYT <robinjratr@gmail.com>
2025-09-24 19:32:20 +0530

    refactor(test): simplify ESM video search output and pagination

--- test/test-esm.mjs
+++ test/test-esm.mjs
@@ -7,85 +7,32 @@ import {
 
 (async function testESM() {
   try {
-    const query = "fejo official channel";
+    const query = "lofi hip hop";
     console.log("=== ESM Video Search Test ===");
-    const res = await searchYouTube(query, {
+    const searchRes = await searchYouTube(query, {
       type: "any",
       limit: 43,
     });
-    console.log("=== First Page === metadata:", res.metadata);
-    console.log("------------------------------------------------------------");
-    console.log("Playlists:", res.playlists.length);
-    // console.log("------------------------------------------------------------");
-    // res.playlists.forEach((v, i) => console.log(i + 1, v.title, v.url));
-    console.log("------------------------------------------------------------");
-    console.log("Videos:", res.videos.length);
-    // console.log("------------------------------------------------------------");
-    // res.videos.forEach((v, i) => console.log(i + 1, v.title, v.url));
-    console.log("------------------------------------------------------------");
-    console.log("Channels:", res.channels.length);
-    console.log("------------------------------------------------------------");
-    // res.channels.forEach((v, i) => console.log(i + 1, v.title, v.url));
-    /*console.log("------------------------------------------------------------");
-    console.log("Movies:", res.movies.length);
-    console.log("------------------------------------------------------------");
-    res.movies.forEach((v, i) => console.log(i + 1, v.title, v.url));
-    console.log("------------------------------------------------------------");
-    console.log("Lives:", res.lives.length);
-    console.log("------------------------------------------------------------");
-    res.lives.forEach((v, i) => console.log(i + 1, v.title, v.url));*/
-    for (let i = 0; i < 5; i++) {
-      if (res.metadata.hasNextPage) {
-        const nextPage = await res.nextPage();
-        if (!nextPage) break;
-        console.log("=== Next Page === metadata:", res.metadata);
-        console.log(
-          "------------------------------------------------------------"
-        );
-        console.log("Playlists:", nextPage.playlists.length);
-        // console.log(
-        //   "------------------------------------------------------------"
-        // );
-        // nextPage.playlists.forEach((v, i) =>
-        //   console.log(i + 1, v.title, v.url)
-        // );
-        console.log(
-          "------------------------------------------------------------"
-        );
-        console.log("Videos:", nextPage.videos.length);
-        // console.log(
-        //   "------------------------------------------------------------"
-        // );
-        // nextPage.videos.forEach((v, i) =>
-        //   console.log(i + 1, v.title, v.url)
-        // );
-        console.log(
-          "------------------------------------------------------------"
-        );
-        console.log("Channels:", nextPage.channels.length);
-        console.log(
-          "------------------------------------------------------------"
-        );
-        // nextPage.channels.forEach((v, i) => console.log(i + 1, v.title, v.url));
-        /*console.log(
-          "------------------------------------------------------------"
-        );
-        console.log("Movies:", nextPage.movies.length);
-        console.log(
-          "------------------------------------------------------------"
-        );
-        nextPage.movies.forEach((v, i) => console.log(i + 1, v.title, v.url));
-        console.log(
-          "------------------------------------------------------------"
-        );
-        console.log("Lives:", nextPage.lives.length);
-        console.log(
-          "------------------------------------------------------------"
-        );
-        nextPage.lives.forEach((v, i) => console.log(i + 1, v.title, v.url));*/
-      }
+
+    console.log("Metadata:", searchRes.metadata);
+    console.log("Videos:", searchRes.videos.length);
+    console.log("Channels:", searchRes.channels.length);
+    console.log("Playlists:", searchRes.playlists.length);
+
+    // Show first 3 videos
+    searchRes.videos
+      .slice(0, 3)
+      .forEach((v, i) => console.log(`${i + 1}. ${v.title} (${v.url})`));
+
+    // Paginate
+    if (searchRes.metadata.hasNextPage) {
+      const nextPage = await searchRes.nextPage();
+      console.log("\n=== Next Page ===");
+      console.log("Videos:", nextPage.videos.length);
+      console.log("Channels:", nextPage.channels.length);
+      console.log("Playlists:", nextPage.playlists.length);
     }
-    /*
+    
     console.log("\n=== ESM Channel Search Test ===");
     const channels = await searchYouTube(query, {
       type: "channel",
@@ -124,7 +71,7 @@ import {
     console.log(
       `${videoDetails.title} | ${videoDetails.viewsShort} Views | ${videoDetails.likesShort} Likes`
     );
-    console.log("All pages fetched successfully.");*/
+    console.log("All pages fetched successfully.");
   } catch (err) {
     console.error("ESM Test Error:", err);
   }
